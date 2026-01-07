6983601fcbcac52b1cc626f6d3de39ee6f07d301
Towfeeq Khan <ask.towfeeq@gmail.com>
2026-01-08 01:40:42 +0530

    Add post list page

---
+++ src/components/SideMenu.jsx
@@ -0,0 +1,73 @@
+import { Link } from "react-router";
+import Search from "./Search";
+
+function SideMenu() {
+  return (
+    <div className="px-4 h-max sticky top-8">
+      <h1 className="mb-4 text-sm font-medium">Search</h1>
+      <Search />
+      <h1 className="mt-8 mb-4 text-sm font-medium">Filter</h1>
+      <div className="flex flex-col gap-2 text-sm">
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="newest"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Newest
+        </label>
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="popular"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Most Popular
+        </label>
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="trending"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Trending
+        </label>
+        <label htmlFor="" className="flex items-center gap-2 cursor-pointer">
+          <input
+            type="radio"
+            name="sort"
+            value="oldest"
+            className="appearance-none w-4 h-4 border-[1.5px] border-blue-800 cursor-pointer rounded-sm bg-white checked:bg-blue-800"
+          />
+          Oldest
+        </label>
+      </div>
+      <h1 className="mt-8 mb-4 text-sm font-medium">Categories</h1>
+      <div className="flex flex-col gap-2 text-sm">
+        <Link className="underline cursor-pointer" to="/posts">
+          All
+        </Link>
+        <Link className="underline cursor-pointer" to="/posts?cat=web-design">
+          Web Design
+        </Link>
+        <Link className="underline cursor-pointer" to="/posts?cat=development">
+          Development
+        </Link>
+        <Link className="underline cursor-pointer" to="/posts?cat=databases">
+          Databases
+        </Link>
+        <Link className="underline cursor-pointer" to="/posts?cat=seo">
+          Search Engines
+        </Link>
+        <Link className="underline cursor-pointer" to="/posts?cat=marketing">
+          Marketing
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default SideMenu;
