5300aff1df6164ddc4b16e158c4b1a0f63599452
Towfeeq Khan <ask.towfeeq@gmail.com>
2026-01-26 10:24:28 +0530

    reset scroll position

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,7 +1,15 @@
-import { Outlet } from "react-router";
+import { useEffect } from "react";
+import { Outlet, useLocation } from "react-router";
 import Navbar from "../components/Navbar";
 
 function MainLayout() {
+  const { pathname } = useLocation(); // Get current route path
+
+  // Scroll to top whenever the path changes
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
   return (
     <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-55">
       <Navbar />
