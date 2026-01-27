028af2000d37b0ecd16ac9ce57f436dfa5e177a6
Towfeeq Khan <ask.towfeeq@gmail.com>
2026-01-27 14:14:00 +0530

    wrap Login button in <SignedOut>

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -41,11 +41,16 @@ function Navbar() {
           <Link to="/">Trending</Link>
           <Link to="/">Most Popular</Link>
           <Link to="/">About</Link>
-          <Link to="/login">
-            <button className="py-2 px-6 rounded-3xl bg-blue-800 text-white">
-              Login
-            </button>
-          </Link>
+          <SignedOut>
+            <Link to="/login">
+              <button className="py-2 px-6 rounded-3xl bg-blue-800 text-white">
+                Login
+              </button>
+            </Link>
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
       </div>
       {/* Desktop Menu */}
